c333d0a56e3a22e1fc10148034c68f3d4e60c002
stephen1k <stephen1kuria@gmail.com>
2025-12-04 17:13:48 +0000

    update subnet typos

--- envs/project/subnets.tf
+++ envs/project/subnets.tf
@@ -13,14 +13,14 @@ resource "aws_subnet" "public_2" {
     vpc_id = aws_vpc.project_vpc.id
     cidr_block = "10.0.2.0/24"
     availability_zone = "${var.aws_region}b"
-    map_public_ip_on_launch - true
+    map_public_ip_on_launch = true
      
     tags = {
         Name = "public-subnet-2"
     }
 }
 
-resourse "aws_subnet" "private_1" {
+resource "aws_subnet" "private_1" {
     vpc_id = aws_vpc.project_vpc.id
     cidr_block = "10.0.10.0/24"
     availability_zone = "${var.aws_region}a"
