3e2fe91cab8214bc9f403395796ddf2fbbbe8554
stephen1k <stephen1kuria@gmail.com>
2025-12-07 23:26:20 +0000

    Create an alb

--- envs/project/security-groups.tf
+++ envs/project/security-groups.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "web_sg" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        security_groups = [aws_security_group.alb_sg.id]
     }
 
     egress {
